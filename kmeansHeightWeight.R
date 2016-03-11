0978f1ad7d6a5f36127fef2fdb7cbe04c7fa674f
uredkar <unmesh_redkar@hotmail.com>
2016-03-11 08:59:09 -0600

    cound not convert kevin murphy's code for kemean, instead using https://github.com/jtan189/kMeansClustering/blob/master/kmeans.R

--- kmeansHeightWeight.R
+++ kmeansHeightWeight.R
@@ -1,4 +1,4 @@
-rm(list(ls))
+rm(list=ls())
 library(ggvis)
 library(caret)
 library(gmodels)
@@ -7,9 +7,10 @@ library(reshape2)
 library(R.matlab)
 library(matlab)
 
-set.seed(1234)
 
+#set.seed(1234)
 
+################################ using built in kmeans
 
 heightWeight.data = readMat("data/heightWeight/heightWeight.mat")
 hw.data = heightWeight.data[[1]]
@@ -24,5 +25,56 @@ plot(X[,1],X[,2], col = cl2$cluster,pch=8:9,xlab = "height", ylab = "weight")
 
 cl3 = kmeans(X,3)
 plot(cl3$centers,col=1:3, pch=8, cex = 2,xlab = "height", ylab = "weight")
-plot(X[,1],X[,2], col = cl3$cluster,pch=8:10,xlab = "height", ylab = "weight")
+plot(X[,1],X[,2], col = cl3$cluster,pch=8,xlab = "height", ylab = "weight")
+
+################### now for code in https://github.com/jtan189/kMeansClustering/blob/master/kmeans.R
+#rm(list=ls())
+source("utils.r")
+K = 3
+X = matrix(X,ncol=2, byrow = FALSE)
+n = dim(X)[1]
+d = dim(X)[2]
+
+
+#mu = kmeansRandomMu(n = nrow(X),x = X,K = K)
+
+## not using this from kevin murphy, sounds like magic
+#xdist = sqDistance(X,mu) 
+
+#### this is easy to understand 
+#xdist = centroidDists(X,mu)
+
+ids = sample(1:n, K)
+centroids = X[ids, ]
+
+plot(X[,1],X[,2],col="blue", xlab = "height", ylab = "weight" )
+
+for(iter in 1:maxIter) {
+  
+  clusters = assignClusters(X, centroids)
+  points(X,col= clusters)
+  points(centroids,col= "black", cex=5)
+  
+  #partMu = partitionedMean(x = X, y = clusters)
+  
+  newCentroids = t(sapply(1:K, function(c) colMeans(X[which(clusters == c), ])))
+  points(newCentroids,col= "orange", cex=5, lwd="2")
+  
+  ## check if change in centroids is significant
+  delta = abs(newCentroids - centroids) 
+  delta_avg = (abs(newCentroids) + abs(centroids) + epsilon)/2
+  
+  cat("Delta:", delta,iter, "\n")
+  if (any(delta / delta_avg > epsilon)) {
+    ## use new centroids for next iteration
+    centroids = newCentroids
+    
+  } 
+  else {
+    break
+  }
+}
+
+plot(X[,1],X[,2],col=centroids, xlab = "height", ylab = "weight" )
+plot(X[,1],X[,2],col=clusters, xlab = "height", ylab = "weight" )
 
--- utils.R
+++ utils.R
@@ -1,8 +1,94 @@
 library(dplyr)
-# convert from utils.py, kevin murphy book
+library(pracma)
+library(matlab)
+library(MASS)
+# convert functions from  kevin murphy book code check github
 set.seed(0)
+epsilon = 1e-4
+maxIter = 1000
+########### rotate a 2d matrix
+rotate <- function(x) {
+  t(apply(x, 2, rev))
+} 
 
-rotate <- function(x) t(apply(x, 2, rev))
+########### get sq of distance from kevin murphy code
+sqDistance = function(p,q)
+{
+  pSOS = apply(p^2,MARGIN = 2, FUN = sum)
+  qSOS = apply(q^2,MARGIN = 2, FUN = sum)
+  a = bsxfun("+",pSOS,qSOS)
+  b = - 2 * (p %*% t(q))
+  a+b
+}
+
+##https://github.com/jtan189/kMeansClustering/blob/master/kmeans.R
+centroidDists <- function(X, c) {
+  ## For each row in the input data, find its Euclidean distance to
+  ## each of the centroids.
+  ##
+  ## Args:
+  ##   X: input data matrix, where each row represents a point
+  ##   c: centroid matrix, where each row represents a centroid
+  ## Returns:
+  ##   A matrix with the same number of rows as X and the same number
+  ##   of columns as c. The rows correspond to the points in
+  ##   X and the columns correspond to the centroids. The value
+  ##   for a particular (row, col) is the Euclidean distance
+  ##   for the corresponding (point, centroid).
+  dists = apply(X, 1, function(point)
+                          sapply(1:nrow(c), function(dim)
+                                                dist(rbind(point, c[dim, ]))))
+  return(t(dists))
+}
+
+##https://github.com/jtan189/kMeansClustering/blob/master/kmeans.R
+assignClusters <- function(X, c) {
+  ## Assign the points in the input data to its closest centroid,
+  ## using Euclidean distances.
+  ##
+  ## Args:
+  ##   X: input data matrix, where each row represents a point
+  ##   c: centroid matrix, where each row represents a centroid 
+  ## Returns:
+  ##   A vector with the same number of rows as X. The value for
+  ##   a given index in this vector is indicates the cluster
+  ##   centered at at c[index, ].
+  
+  n = dim(X)[1]
+  d = dim(X)[2]
+  
+  cDists = centroidDists(X, c)
+  clusterAndDist = sapply(1:n, function(x) which.min(cDists[x, ]))
+  return(clusterAndDist)
+}
+
+### returns a list with index of vector for which the value in the vector is the minimum
+minidx = function(x)
+{
+  
+  idx = apply(x,MARGIN = 2, FUN = which.min)
+  minvalue = x[idx]
+  list(idx = idx, minvalue = minvalue)
+}
+
+# x input vector, y the class labels, returns a list of class tables and mean value in each class 
+partitionedMean = function(x,y)
+{
+  grp_by = dplyr::group_by(x,y)
+  dplyr::summarise(grp_by,mean(x))
+}
+
+
+kmeansRandomMu = function(n,x,K=2)
+{
+  perm = pracma::randperm(n)
+  rv = apply(x,MARGIN = 2,FUN =  stats::var)  # var of each col
+  rmu = matlab::zeros(1,length(rv))
+  noise = MASS::mvrnorm(n = K, mu = rmu , Sigma= 0.01 * diag(rv))
+  t(x[perm[1:K],]) + t(noise)
+}
+
+
 
 poly_data_make = function(sampling="sparse", deg=3, n=21)
 {
